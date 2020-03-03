5eb86d9a818cd956b6b54d1197dda18e9a8fc0e8
Matteo <m.alberti@deeplearningitalia.com>
2020-03-03 19:17:06 +0100

    fix generation of container

---
+++ generate_containers.sh
@@ -0,0 +1,14 @@
+#! bin/bash/
+
+cd /home/albertilabs/MHPC_Thesis_NextCloud/benchmarks/Mlperf_training/Singularity
+
+dir=sif
+
+
+python3 generate_container.py $@
+
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+fi
+
+sudo singularity build $dir/Singularity_tensorflow_base.sif Singularity_tensorflow_base.def
